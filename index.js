8ca489d547552c04ef7cfb7df9bab95f303e58c4
lmuhamadaslan <muhamadaslan22@gmail.com>
2022-11-05 09:32:11 +0800

    initial commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Import Routes
 import ActivityRoutes from "./routes/ActivityRoute.js";
